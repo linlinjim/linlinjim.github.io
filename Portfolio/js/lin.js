32cebbf58d6e60712ebd467f4c1e4b1d7d9c5da1
linlinjim <117882564+linlinjim@users.noreply.github.com>
2022-11-10 17:35:54 +0800

    no1

---
+++ Portfolio/js/lin.js
@@ -0,0 +1,11 @@
+function jsyy() {
+    document.getElementById('yy').style.visibility="initial"
+}
+
+function jsnn() {
+    document.getElementById('yy').style.visibility="hidden"
+}
+
+function hello() {
+    alert('你好 很高興認識你');
+}
